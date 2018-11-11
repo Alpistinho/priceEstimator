72403c1e74fb222d3428fcb688cf9d6e3aacc718
Daniel Moreira Guimarães <danielmguimaraes@poli.ufrj.br>
2018-11-11 13:48:11 -0200

    Helper functions for outlier analysis and removal

--- estimatePrice.py
+++ estimatePrice.py
@@ -8,11 +8,16 @@ from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold, StratifiedKFold
 
 from sklearn.linear_model import Ridge
 
 import analysisPlotter
+
+def outputResult(outfile):
+
+    with open(outfile , 'w') as f:
+        f.write('Id,preco')
 
 def rmspe(correct, prediction):
     length = np.min([len(correct),len(prediction)])
@@ -24,7 +29,19 @@ def rmspe(correct, prediction):
         totalError += error
         errors.append(error)
     totalError = np.sqrt(totalError/length)
-    return totalError, np.array(errors)
+    return totalError, errors
+    
+def printResults(results, print_coefs=False):
+    for key in results.keys():
+        print()
+        print(key)
+        if print_coefs:
+            print(results[key]['coef'])
+        print('\nDesempenho no conjunto de treinamento:')
+        print('RMSPE = %.3f' % results[key]['rmspe_train'])
+
+        print('\nDesempenho no conjunto de teste:')
+        print('RMSPE = %.3f' % results[key]['rmspe_test'])
 
 if __name__ == '__main__':
 
@@ -32,6 +49,8 @@ if __name__ == '__main__':
     
     # input data
     dataset = dataset.loc[:, dataset.columns != 'diferenciais']
+    x2 = dataset[np.abs(dataset.preco - dataset.preco.mean()) <= (4*dataset.preco.std())]
+    dataset = x2
     x = dataset.iloc[:,1:-1].values
     # output data
     y = dataset['preco'].values
@@ -61,10 +80,13 @@ if __name__ == '__main__':
     y_pred_train = linearRegressor.predict(x_train)
     y_pred_test = linearRegressor.predict(x_test)
 
-    lr_ridge = Ridge(alpha=1000)
+    lr_ridge = Ridge(alpha=10000)
     lr_ridge.fit(x_train, y_train)
     y_pred_train_ridge = lr_ridge.predict(x_train)
     y_pred_test_ridge = lr_ridge.predict(x_test)
+
+
+    results = {}
 
     rmspe_train, errors_train = rmspe(y_train, y_pred_train)
     rmspe_test, errors_test = rmspe(y_test , y_pred_test)
@@ -72,24 +94,19 @@ if __name__ == '__main__':
     rmspe_train_ridge, errors_train_ridge = rmspe(y_train, y_pred_train_ridge)
     rmspe_test_ridge, errors_test_ridge = rmspe(y_test , y_pred_test_ridge)
 
-    print('LinearRegression')
-    print(linearRegressor.coef_)
-    print('\nDesempenho no conjunto de treinamento:')
-    print('RMSPE = %.3f' % rmspe_train)
+    results['LinearRegression'] = {'rmspe_train': rmspe_train, 'rmspe_test': rmspe_test, 'coef': linearRegressor.coef_}
+    results['LinearRegression with Rigde'] = {'rmspe_train': rmspe_train_ridge, 'rmspe_test': rmspe_test_ridge, 'coef': lr_ridge.coef_}
 
-    print('\nDesempenho no conjunto de teste:')
-    print('RMSPE = %.3f' % rmspe_test)
+    printResults(results)
 
-    print('LinearRegression with Rigde')
-    print(lr_ridge.coef_)
-    print('\nDesempenho no conjunto de treinamento:')
-    print('RMSPE = %.3f' % rmspe_train_ridge)
-
-    print('\nDesempenho no conjunto de teste:')
-    print('RMSPE = %.3f' % rmspe_test_ridge)
-
-    
-    
     analysisPlotter.plotAscending(x_train[:,75], y_pred_train)
     analysisPlotter.plotAscending(x_train[:,75], y_pred_train_ridge)
+    analysisPlotter.plotAscending(y_pred_train_ridge, np.array(errors_train_ridge))
+
+    import heapq
+
+    largest = heapq.nlargest(6, errors_train_ridge)
+    for large in largest:
+        print(errors_train_ridge.index(large))
+    # analysisPlotter.plotHistogram(errors_train_ridge)
     plt.show()
